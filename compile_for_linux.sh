c51a5440bcb4e756283c6d8dbcaeee5e302ff466
Chester Gillon <chester.gillon@metronet.co.uk>
2022-06-19 17:43:45 +0100

    Change switch_test to take a command line options for the requested bit rate (in Mbps) on each port of the switch under test, and bit rate of the interface to the injection switch. These are used to determine: a. If need to limit the transmit frame rate. b. If the frame rate needs to be limited what rate to use to avoid exceeding the requested bit rate on each switch port under test.
    While the test is running also reports the average bit rate achieved on each switch port under test.
    
    This change makes the program easier to use, and avoids the previous hard-coded assumptions in the programs of the network port speeds.

--- compile_for_linux.sh
+++ compile_for_linux.sh
@@ -18,7 +18,7 @@ do
         build_configuration_flags="-O3"
         ;;
   esac
-  gcc -Wall -std=gnu99 ${build_root}/switch_test_main.c -o ${exe_dir}/switch_test -l pcap -l rt -l pthread ${build_configuration_flags}
+  gcc -Wall -std=gnu99 ${build_root}/switch_test_main.c -o ${exe_dir}/switch_test -l m -l pcap -l rt -l pthread ${build_configuration_flags}
   echo "Setting permission for switch_test to send/receive packets"
   sudo setcap cap_net_admin,cap_net_raw+eip ${exe_dir}/switch_test
 done
